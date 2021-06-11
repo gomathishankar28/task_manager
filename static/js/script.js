e1660e99e5b8d676203855eaeb9817e4f7a61c58
gomathishankar28 <gomathishankar28@gmail.com>
2021-06-11 20:58:26 +0000

    created add_task.html with datepicker and dropdown from materialize

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-    $('.collapsible').collapsible();
+    $(".collapsible").collapsible();
     $(".tooltipped").tooltip();
+    $("select").formSelect();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
+
